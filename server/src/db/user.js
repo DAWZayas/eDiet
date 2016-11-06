9430724155ae62c00ab57d0538f78f5dac2ebe27
Andrea Sanz <asanzdj@gmail.com>
2016-11-06 14:09:13 +0100

    Updated user.js to us

--- server/src/db/user.js
+++ server/src/db/user.js
@@ -1,9 +1,14 @@
 import {thinky} from './thinky';
 
 const User = thinky.createModel('User', {
-  email: thinky.type.string().required(),
-  login: thinky.type.string().required(),
-  password: thinky.type.string().required(),
+  name: thinky.type.string().required(),
+  surname: thinky.type.string(),
+  weight: thinky.type.number().required(),
+  height: thinky.type.number().required(),
+  age: thinky.type.number().integer().required(),
+  email: thinky.type.string().email().required(),
+  id: thinky.type.string().required(),
+  password: thinky.type.string().alphanum().required(),
   registrationDate: thinky.type.date().default(thinky.r.now()),
 });
 
