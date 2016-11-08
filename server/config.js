1d0e52a024b3834ea107d39b7919a42914d14b18
Andrea Sanz <asanzdj@gmail.com>
2016-11-08 16:49:35 +0100

    Send JWT back after login and his test

--- server/config.js
+++ server/config.js
@@ -7,4 +7,5 @@ export const db = {
 export const auth = {
   passwordSalt: '3^V5.d%6GbjJe3998R6H2q~8sSdb.t7:|QW59U:gg;Hb%_tp-;!u--q*wN4.6!7A',
   sessionSecret: 'cdFAu7s!gsV^*%!=%!H-.^.^5=p%:_U4^:~c4j~-EDmE+%*;unkb;K-!+_~-%!.g',
+  jwtSecret: '.q$bd<$Y0j&(AC*n#Exu2rntt`()KmV]VTF^gD5w]XCD-&=sU>%I9[[?VvV]rC1p',
 };
