3ebb79ad73b973fbcae0d7334d0b4f8aa16c0e31
Andrea Sanz <asanzdj@gmail.com>
2017-01-08 15:32:40 +0100

    [server] Updated get in exercises

--- server/src/exerciseTable/exercises/get.js
+++ server/src/exerciseTable/exercises/get.js
@@ -3,20 +3,52 @@ import {Exercise} from '../../db';
 import {asyncRequest} from '../../util';
 
 export default (app) => {
-  app.post('/api/exercise/:id/get', asyncRequest(async (req, res) => {
-    try {
-      const {id} = req.params;
-      const {name} = req.body;
+  app.post('/api/exercise/:name/get', asyncRequest(async (req, res) => {
+    // Sacar 1 ejercicio concreto
+    let tables;
+    let table;
+    let exercise;
+    const {name} = req.body;
 
-      const exerciseTable = await Exercise.get(id);
-      const exercises = exerciseTable.exercises.filter(function (obj) {
-        return obj.name === name ? true : false;
-      });
-      const exercise = exercises.reduce(function(a,b) {a.concat(b);});
-      console.log(exercise);
-      res.send(exercise);
+    try {
+      tables = await Exercise;
+      table = tables.filter(table => table.name === req.params.name).reduce((a, b) => a.concat(b));
+
     } catch (e) {
-      res.status(400).send({error: 'Exercise does not exist'});
+      res.status(400).send({error: 'No se ha encontrado la tabla'});
     }
+
+    try {
+      exercise = table.exercises.filter(exercise => exercise.name === req.body.name).reduce((a, b) => a.concat(b));
+
+    } catch (e) {
+      res.status(400).send({error: 'No se ha encontrado el ejercicio'});
+    }
+
+    res.send(exercise);
+  }));
+
+  app.get('/api/exercises/:name/get', asyncRequest(async (req, res) => {
+    // Sacar todos los ejercicios de una tabla
+    let tables;
+    let table;
+    let exercises;
+
+    try {
+      tables = await Exercise;
+      table = tables.filter(table => table.name === req.params.name).reduce((a, b) => a.concat(b));
+
+    } catch (e) {
+      res.status(400).send({error: 'No se ha encontrado la tabla'});
+    }
+
+    try {
+      exercises = table.exercises;
+
+    } catch (e) {
+      res.status(400).send({error: 'No se han encontrado ejercicios'});
+    }
+
+    res.send(exercises);
   }));
 };
