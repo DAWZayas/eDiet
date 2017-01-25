857f2a125a8b6d708a446a949f0772124b5f05d7
Andrea Sanz <asanzdj@gmail.com>
2017-01-25 11:13:05 +0100

    [client] Change some styles in profile

--- client/src/pages/user/index.js
+++ client/src/pages/user/index.js
@@ -13,6 +13,13 @@ const mapStateToProps = (state) => ({
    user: state.user.user,
  });
 
+ const style = {
+   header: {
+      borderBottom: 'none',
+      backgroundColor: 'rgba(232, 142, 58, 0.33)',
+   }
+ };
+
  const mapDispatchToProps = (dispatch) => ({
    getUser:  payload => dispatch(getUserAction(payload)),
    navToUser: () => dispatch(push('/user/updateProfile')),
@@ -29,104 +36,110 @@ class user extends React.Component {
   }
 
   render(){
-  return (
-    <div className={`container ${styles.container}`}>
-      <center>
-        <div className={`${styles.avatar}`}>
-          <img src='../../../images/logo/logo.png'/>
-            {
-              user ?
-                user.login ?
-                <p>
-                  {user.login}
-                </p>
+    const {user} = this.props;
+
+    return (
+      <div className={`container ${styles.container}`}>
+        <center>
+          <div className={`${styles.avatar}`}>
+            <img src='../../../images/logo/logo.png'/>
+              {
+                user ?
+                  user.login ?
+                  <p>
+                    {user.login}
+                  </p>
+                  : null
                 : null
+              }
+          </div>
+        </center>
+        <div className={`${styles.userButtons}`}>
+          <Link to='/user/updateProfile' style={{margin: '0 2% 0 2%'}}>
+            <button className="btn">
+              Update profile
+            </button>
+          </Link>
+          {
+            user ?
+              !user.role ?
+                <Link to='/user/graphics' style={{margin: '0 2% 0 2%'}}>
+                  <button className="btn">
+                      Progress
+                  </button>
+                  </Link>
               : null
-            }
-        </div>
-      </center>
-      <div className={`${styles.userButtons}`}>
-        <button className="btn btn-default">
-          <Link to='/user/updateProfile'>
-            Update profile
-          </Link>
-        </button>
-        <button className="btn btn-default">
-          <Link to='/user/graphics'>
-            Progress
-          </Link>
-        </button>
-        {
-          user ?
-            user.role ?
-              <button className="btn btn-default">
-                <Link to='/user/addAdmin'>
-                  <i className={`fa fa-plus`} aria-hidden="true"></i>
-                  Admin. user
-                </Link>
-              </button>
             : null
-          : null
-        }
-      </div>
-      <div className={`panel panel-default ${styles.panel}`}>
-        <div className="panel-heading">
-          PROFILE
+          }
+          {
+            user ?
+              user.role ?
+                <Link to='/user/addAdmin' style={{margin: '0 2% 0 2%'}}>
+                  <button className="btn">
+                    <i className={`fa fa-plus`} aria-hidden="true"></i>
+                    Admin. user
+                  </button>
+                </Link>
+              : null
+            : null
+          }
         </div>
-        <div className="panel-body">
-          {user ?
-              <div>
-                <div className={`${styles.data}`}>
-                  <p>Email:</p>
-                  {user.email ?
-                    <p>{user.email}</p>
-                  :
-                    <p>Without email associated</p>
-                  }
-                </div>
-                <div className={`${styles.data}`}>
-                  <p>Register date:</p>
-                  {user.registrationDate ?
-                    <p>{user.registrationDate}</p>
-                  :
-                    <p>Without register date associated</p>
-                  }
-                </div>
-                {!user.role ?
+        <div className={`panel panel-default ${styles.panel}`}>
+          <div className="panel-heading" style={style.header}>
+            PROFILE
+          </div>
+          <div className="panel-body">
+            {user ?
+                <div>
                   <div className={`${styles.data}`}>
-                    <p>Height:</p>
-                    {user.height ?
-                      <p>{user.height}</p>
+                    <p className={`${styles.tittle}`}>Email:</p>
+                    {user.email ?
+                      <p className={`${styles.result}`}>{user.email}</p>
                     :
-                      <p>Without height associated</p>
+                      <p className={`${styles.result}`}>Without email associated</p>
                     }
                   </div>
-                : null}
-              </div>
+                  <div className={`${styles.data}`}>
+                    <p className={`${styles.tittle}`}>Register date:</p>
+                    {user.registrationDate ?
+                      <p className={`${styles.result}`}>{user.registrationDate}</p>
+                    :
+                      <p className={`${styles.result}`}>Without register date associated</p>
+                    }
+                  </div>
+                  {!user.role ?
+                    <div className={`${styles.data}`}>
+                      <p className={`${styles.tittle}`}>Height:</p>
+                      {user.height ?
+                        <p className={`${styles.result}`}>{user.height}</p>
+                      :
+                        <p className={`${styles.result}`}>Without height associated</p>
+                      }
+                    </div>
+                  : null}
+                </div>
+              : null}
+            </div>
+            {user ?
+              !user.role ?
+                <div className={`${styles.dietButtons}`}>
+                  <button className="btn">
+                    <Link to='#'>
+                      See my menus
+                    </Link>
+                  </button>
+                  <button className="btn">
+                    <Link to='#'>
+                      See my exercises
+                    </Link>
+                  </button>
+                </div>
+              : null
             : null}
           </div>
-          {user ?
-            !user.role ?
-              <div className={`${styles.dietButtons}`}>
-                <button className="btn btn-default">
-                  <Link to='#'>
-                    See my menus
-                  </Link>
-                </button>
-                <button className="btn btn-default">
-                  <Link to='#'>
-                    See my exercises
-                  </Link>
-                </button>
-              </div>
-            : null
-          : null}
         </div>
-      </div>
-    )
-  }
+      )
+    }
  };
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(user);
