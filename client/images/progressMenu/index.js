d00a1a0a4931541cb5392cee7ac271523621155a
oscar.rodriguez094@gmail.com <oscar.rodriguez094@gmail.com>
2017-01-30 16:49:02 +0100

    new progress component

--- client/images/progressMenu/index.js
+++ client/images/progressMenu/index.js
@@ -2,8 +2,8 @@
 let images;
 
 export default images = [
-  {name: 'American', Image:'../../../images/progressMenu/american.jpg', level: 1 },
-  {name: 'Francess', Image:'../../../images/progressMenu/francesa.jpg', level: 2 },
-  {name: 'China', Image:'../../../images/progressMenu/sushi.jpg', level: 4 },
-  {name: 'Mediterranea', Image:'../../../images/progressMenu/mediterranea.jpg', level: 3 },
+  {name: 'American', image:'../../../images/progressMenu/american.jpg', level: 1 },
+  {name: 'Francess', image:'../../../images/progressMenu/francesa.jpg', level: 2 },
+  {name: 'China', image:'../../../images/progressMenu/sushi.jpg', level: 4 },
+  {name: 'Mediterranea', image:'../../../images/progressMenu/mediterranea.jpg', level: 3 },
 ];
