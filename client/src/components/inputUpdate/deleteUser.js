8afbcc226bda3b8558dde23971aa743e71195105
Andrea Sanz <asanzdj@gmail.com>
2017-02-04 12:44:27 +0100

    [client] Fixed error in merge files -> now delete user works fine

--- client/src/components/inputUpdate/deleteUser.js
+++ client/src/components/inputUpdate/deleteUser.js
@@ -2,27 +2,21 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {push} from 'react-router-redux';
 
-import {deleteUserAction} from '../../store/actions';
-
-const mapStateToProps = state => ({
-  redirectToLogin: state.user.navToLogin
-});
+import {deleteUserAction, logoutAction} from '../../store/actions';
 
 const mapDispatchToProps = (dispatch) => ({
   navToLogin: () => dispatch(push('/login')),
   deleteUser: payload => dispatch(deleteUserAction(payload)),
+  logOut: () => dispatch(logoutAction())
 });
 
-const deleteUser = ({deleteUser, id, redirectToLogin, navToLogin}) => {
-  console.log('>>>Nav', navToLogin)
+const deleteUser = ({deleteUser, id, logOut, navToLogin}) => {
   const handleDeleteUser = (e) => {
     e.preventDefault();
     deleteUser({id});
-   };
-
-  if(redirectToLogin){
+    logOut();
     navToLogin();
-  }
+   };
 
   return (
     <div>
@@ -34,4 +28,4 @@ const deleteUser = ({deleteUser, id, redirectToLogin, navToLogin}) => {
   );
 }
 
-export default connect (mapStateToProps, mapDispatchToProps)(deleteUser);
+export default connect (null, mapDispatchToProps)(deleteUser);
