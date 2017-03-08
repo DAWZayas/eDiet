8bc623ff5606767102a3a5e3c5ec3be81569e80a
Andrea Sanz <asanzdj@gmail.com>
2017-03-08 16:14:16 +0100

    [client] Hidden height update to admin user

--- client/src/components/inputUpdate/updateHeight.js
+++ client/src/components/inputUpdate/updateHeight.js
@@ -24,24 +24,32 @@ class updateHeight extends Component {
 
     return (
       <div className={`${styles.element}`}>
-        <h2>Height</h2>
-        <div>
-          <div className="col-xs-12">
-            <input
-              type="number"
-              className="form-control"
-              id="newHeight"
-              placeholder="New height (in metres)..."
-              ref={(i) => { newHeight = i; }}
-              defaultValue={user.height ? user.height : 0}
-              min='0'
-            />
-          </div>
-          <center>
-          <ModalHeight id={user.id} height={height} update={updateHeight} />
-          </center>
+        {user ?
+          !user.role ?
+          <span>
+            <h2>
+              Height
+            </h2>
+            <div>
+              <div className="col-xs-12">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="newHeight"
+                  placeholder="New height (in metres)..."
+                  ref={(i) => { newHeight = i; }}
+                  defaultValue={user.height ? user.height : 0}
+                  min='0'
+                />
+              </div>
+              <center>
+              <ModalHeight id={user.id} height={height} update={updateHeight} />
+              </center>
+            </div>
+            </span>
+            : null
+          : null}
         </div>
-      </div>
     );
   }
 }
